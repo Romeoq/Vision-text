0efda05252b30cb832480c1496448a73e47dcf93
Romeoq <romeoq.work@gmail.com>
2020-12-10 09:49:05 +0200

    invoices added

--- SwiftOcrTest/SwiftOcrTest/ViewController.swift
+++ SwiftOcrTest/SwiftOcrTest/ViewController.swift
@@ -5,12 +5,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var stackOfImages: UIStackView!
+    
+    private var imagesToScan = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupImages()
+//        setRandomImage()
+        
     }
-
-
+    
 }
 
+private extension ViewController {
+    func setupImages() {
+        for iView in stackOfImages.arrangedSubviews {
+            if let imageView = iView as? UIImageView, let image = imageView.image {
+                imagesToScan.append(image)
+            }
+        }
+    }
+    
+    func setRandomImage() {
+        let iView = UIImageView(frame: UIScreen.main.bounds)
+        iView.image = imagesToScan.randomElement()
+        self.view.addSubview(iView)
+    }
+}
