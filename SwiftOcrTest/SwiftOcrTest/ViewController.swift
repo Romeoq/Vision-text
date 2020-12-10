d00cbdea5cbe70701c1287dab1d1301289d2feec
Romeoq <romeoq.work@gmail.com>
2020-12-10 09:17:52 +0200

    start

---
+++ SwiftOcrTest/SwiftOcrTest/ViewController.swift
@@ -0,0 +1,16 @@
+// Created by Roman Voinitchi on 12/10/20
+// 
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
