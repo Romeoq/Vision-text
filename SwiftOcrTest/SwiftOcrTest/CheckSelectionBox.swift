cce022ee1c041fb186c8ec563f86c8df03cac08a
Romeoq <romeoq.work@gmail.com>
2020-12-22 13:45:09 +0200

    before buttons

---
+++ SwiftOcrTest/SwiftOcrTest/CheckSelectionBox.swift
@@ -0,0 +1,13 @@
+// Created by Roman Voinitchi on 12/22/20
+// 
+
+
+import Foundation
+import Vision
+
+struct CheckSelectionBox {
+    
+    let double: Double
+    let boundingBox: CGRect
+    
+}
--- SwiftOcrTest/SwiftOcrTest/ViewController.swift
+++ SwiftOcrTest/SwiftOcrTest/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     let textRecognitionWorkQueue = DispatchQueue(label: "TextRecognitionQueue", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
     
     private var loaderView: UIView?
+    private var boxes = [CheckSelectionBox]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,18 +53,26 @@ private extension ViewController {
             guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
 
             var detectedText = ""
+            self.boxes.removeAll()
             for observation in observations {
                 guard let topCandidate = observation.topCandidates(1).first else { return }
-                detectedText += topCandidate.string
+                var finalString = topCandidate.string.replacingOccurrences(of: ",", with: ".")
+                finalString = finalString.replacingOccurrences(of: "-", with: "")
+                detectedText += finalString
                 detectedText += "\n"
+                
+                if let double = Double(finalString) {
+                    self.boxes.append(CheckSelectionBox(double: double, boundingBox: observation.boundingBox))
+                }
             }
             
             DispatchQueue.main.async {
                 self.removeLoader()
+                self.drawBoxes()
                 self.textView.text += detectedText
             }
         }
-        textRecognitionRequest.minimumTextHeight = 0.013
+        textRecognitionRequest.minimumTextHeight = 0.011
         textRecognitionRequest.recognitionLevel = .accurate
         textRecognitionRequest.usesLanguageCorrection = false
         textRecognitionRequest.recognitionLanguages = ["ru", "en", "de"]
@@ -78,5 +87,24 @@ private extension ViewController {
     func removeLoader() {
         self.loaderView?.removeFromSuperview()
         self.loaderView = nil
+    }
+    
+    func drawBoxes() {
+        guard let image = checkImageView.image else  { return }
+        
+        let imageTransform = CGAffineTransform.identity.scaledBy(x: 1, y: -1).translatedBy(x: 0, y: -image.size.height).scaledBy(x: image.size.width, y: image.size.height)
+        
+        UIGraphicsBeginImageContextWithOptions(image.size, false, 1.0)
+        let context = UIGraphicsGetCurrentContext()!
+        image.draw(in: CGRect(origin: .zero, size: image.size))
+        context.setStrokeColor(CGColor(srgbRed: 1, green: 0, blue: 0, alpha: 1))
+        context.setLineWidth(2)
+        for box in boxes {
+            context.addRect(box.boundingBox.applying(imageTransform))
+        }
+        context.strokePath()
+        let result=UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        checkImageView.image = result
     }
 }
